41a05f11fd054c60bb7296c91317b2bf0e22c651
DavidDengHui <david.no.1@msn.cn>
2023-04-01 00:14:13 +0800

    修改备案信息

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer class="mod-footer" role="contentinfo" id="footer_in">
 	<p class="f_bq">
-		<?php //if($word_t2!=""){echo $word_t2;}else{echo 'Copyright';} ?>Copyright &copy; 2014-<?php echo date("Y"); echo " <a class='banquan' title='";bloginfo('name');echo '：'; bloginfo('description');echo "' target='_blank' href='//covear.top/'>"; bloginfo('name'); echo '</a><a title="View Information" href="http://icp.chinaz.com/info?q=covear.top" target="_blank"> · </a><a href="http://beian.miit.gov.cn/" target="_blank">湘ICP备17019987号</a>'; ?></p>
+		<?php //if($word_t2!=""){echo $word_t2;}else{echo 'Copyright';} ?>Copyright &copy; 2014-<?php echo date("Y"); echo " <a class='banquan' title='";bloginfo('name');echo '：'; bloginfo('description');echo "' target='_blank' href='//covear.top/'>"; bloginfo('name'); echo '</a><a title="View Information" href="https://icp.chinaz.com/home/info?host=covear.top" target="_blank"> · </a><a href="http://beian.miit.gov.cn/" target="_blank">湘ICP备17019987号</a>'; ?></p>
 </footer>
 <script>
 POWERMODE.colorful = true; // ture 为启用礼花特效
-POWERMODE.shake = false; // false 为禁用震动特效
+POWERMODE.shake = true; // false 为禁用震动特效
 document.body.addEventListener('input', POWERMODE);
 var com = document.getElementById('comment');
 if ( com != null ) {
